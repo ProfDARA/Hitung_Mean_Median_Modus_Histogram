d71df8b1d6ee8cb48055c1cf963d529544c0a992
Danang Agung Restu Aji <134141535+ProfDARA@users.noreply.github.com>
2024-05-05 15:56:58 +0700

    Create koef_keragaman.R

---
+++ koef_keragaman.R
@@ -0,0 +1,15 @@
+# Nilai mahasiswa 2023
+nilai_2022 <- c(88) 
+simpangan_standar_2022 <- 5
+rata_rata_2022 <- mean(nilai_2022)
+cv_2022 <- (simpangan_standar_2022 / rata_rata_2022) * 100
+print(cv_2022)
+
+# Nilai mahasiswa 2023
+nilai_2023 <- c(77)
+simpangan_standar_2023 <- 6
+rata_rata_2023 <- mean(nilai_2023)
+cv_2023 <- (simpangan_standar_2023 / rata_rata_2023) * 100
+print(cv_2023)
+
+#Koef keragaman = (SD/Rata)*100
